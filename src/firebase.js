b0b918528f7c154673a8afcda883e2457e01711f
grahamprice <105814543+grahamprice@users.noreply.github.com>
2023-01-26 14:24:49 -0700

    fixed originals

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdxCWxVVY9OvxXZGhZlvX7kMwABxOjWfA",
+  authDomain: "netflix-build-graham.firebaseapp.com",
+  projectId: "netflix-build-graham",
+  storageBucket: "netflix-build-graham.appspot.com",
+  messagingSenderId: "882572978163",
+  appId: "1:882572978163:web:732ecde12758b081655486",
+};
